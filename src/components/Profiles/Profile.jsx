8eaf3d54296463c4d48561dafcdefc4c273ba63e
shakizWasTaken <yahya2003rhemmali@gmail.com>
2022-04-29 20:19:35 +0000

    initial

---
+++ src/components/Profiles/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Profile = ()=>{
+    
+    return(
+        <>
+        
+            
+        
+        </>
+    );
+
+}
+
+
+export default Profile;
---
+++ src/components/Profiles/ProfilesHandler.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+let profiles = [
+
+    {
+
+    }
+];
+
+const ProfilesHandler = ()=>{
+    return(
+        <>
+        </>
+    );
+}
+
+export default ProfilesHandler;
+
